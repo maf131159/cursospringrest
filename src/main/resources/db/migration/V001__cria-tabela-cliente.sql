2157c8514201c971889c3590e3dedd6e5fd590b2
maf131159 <manoelantoniofernandes@gmail.com>
2022-01-13 20:08:31 -0300

    Aula 2 - Usando Jakarta Persistence (JPA)

--- src/main/resources/db/migration/V001__cria-tabela-cliente.sql
+++ src/main/resources/db/migration/V001__cria-tabela-cliente.sql
@@ -2,7 +2,7 @@ create table cliente (
 	id bigint not null auto_increment,
     nome varchar(60) not null,
     email varchar(255) not null,
-    telefone varchar(2) not null,
+    telefone varchar(25) not null,
     
     primary key (id)
 );
